07f114395f85c81a2b9270b4293ec7a68acc9779
Arthur Pinheiro <arthurpm@id.uff.br>
2020-12-04 13:49:06 -0300

    3

---
+++ 3/src/App.java
@@ -0,0 +1,12 @@
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.ArrayDeque;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedHashMap<String, String> musicas = new LinkedHashMap<String, String>();
+        ArrayDeque<String> fila = new ArrayDeque<String>();
+        fila.removeFirst();
+        fila.add();
+    }
+}
